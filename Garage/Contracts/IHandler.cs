8215241d704c61c0abb0df1a4e47bef3208b71d1
Simon Ektjärn <debarquer@gmail.com>
2024-09-30 10:04:55 +0200

    chore: Add comments

--- Garage/Contracts/IHandler.cs
+++ Garage/Contracts/IHandler.cs
@@ -2,12 +2,47 @@
 
 internal interface IHandler<T> where T : IVehicle
 {
+    /// <summary>
+    /// The capacity of the garage. Readonly.
+    /// </summary>
     public int Capacity { get; }
 
-    public T GetVehicle(int registration);
+    /// <summary>
+    /// Returns a vehicle matching the registration.
+    /// </summary>
+    /// <param name="registration"></param>
+    /// <returns>The vehicle matching the registration.</returns>
+    public T GetVehicle(string registration);
+
+    /// <summary>
+    /// Returns all vehicles in the list.
+    /// </summary>
+    /// <returns>All vehicles in the list.</returns>
     public T[] GetAllVehicles();
-    public T[] GetAllVehicles(Func<T, bool> condition);
-    public void AddVehicle(T registration);
-    public void RemoveVehicle(T registration);
-    public bool HasVehicle(T registration);
+
+    /// <summary>
+    /// Returns all vehicles in the list that match the pattern.
+    /// </summary>
+    /// <param name="condition"></param>
+    /// <returns>The vehicles in the list that match the pattern.</returns>
+    public T[] GetAllVehicles(Func<T, bool> pattern);
+
+    /// <summary>
+    /// Adds a vehicle to the garage.
+    /// </summary>
+    /// <param name="vehicle"></param>
+    public void AddVehicle(T vehicle);
+
+    /// <summary>
+    /// Removes a vehicle from the garage.
+    /// </summary>
+    /// <param name="registration"></param>
+    public void RemoveVehicle(string registration);
+
+    /// <summary>
+    /// Returns true if any vehicles in the garage matches the registration, otherwise returns false.
+    /// </summary>
+    /// <param name="registration"></param>
+    /// <returns>If any vehicles in the garage matches the registration.</returns>
+    public bool HasVehicle(string registration);
 }
--- Garage/Contracts/IUI.cs
+++ Garage/Contracts/IUI.cs
@@ -2,5 +2,9 @@
 
 internal interface IUI
 {
+    /// <summary>
+    /// Prints a message to the user.
+    /// </summary>
+    /// <param name="message"></param>
     public void PrintMessage(string message);
 }
